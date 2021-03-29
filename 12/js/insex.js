ee95b37730e8bd8f44f68734ca50c16251e8e2b3
”Mikhail <mikhail.hasilau@gmail.com>
2021-03-29 16:10:53 +0300

    everysing is wronggit add .!

---
+++ 12/js/insex.js
@@ -0,0 +1,115 @@
+const zero = document.getElementById("zero");
+const one = document.getElementById("one");
+const two = document.getElementById("two");
+const tree = document.getElementById("tree");
+const four = document.getElementById("four");
+const five = document.getElementById("five");
+const six = document.getElementById("six");
+const seven = document.getElementById("seven");
+const eight = document.getElementById("eight");
+const nine = document.getElementById("nine");
+const backspace = document.getElementById("backspace");
+const clearAll = document.getElementById("clearAll");
+const clear = document.getElementById("clear");
+const plusMinus = document.getElementById("plusMinus");
+const sqrtButt = document.getElementById("sqrtButt");
+const split = document.getElementById("split");
+const percent = document.getElementById("percent");
+const multiply = document.getElementById("multiply");
+const splitX = document.getElementById("splitX");
+const minus = document.getElementById("minus");
+const equally = document.getElementById("equally");
+const dot = document.getElementById("dot");
+const plus = document.getElementById("plus");
+
+const variables = document.getElementById("vars");
+const result = document.getElementById("result");
+
+
+function zeroClick() {
+    vars.innerHTML = zero.value;
+    // variables.innerHTML = zero.value;
+    // result.innerHTML = zero.value;
+    console.log(zero.value);
+
+    
+}
+
+function oneClick() {
+    variables.innerHTML = one.value;
+    console.log(one.value);
+}
+
+function twoClick() {
+    variables.innerHTML = two.value;
+    console.log(two.value);
+}
+
+function treeClick() {
+    variables.innerHTML = tree.value;
+    console.log(tree.value);
+}
+
+function fourClick() {
+    variables.innerHTML = four.value;
+    console.log(four.value);
+}
+
+function fiveClick() {
+    variables.innerHTML = five.value;
+    console.log(five.value);
+}
+
+function sixClick() {
+    variables.innerHTML = six.value;
+    console.log(six.value);
+}
+
+function sevenClick() {
+    variables.innerHTML = seven.value;
+    console.log(seven.value);
+}
+
+function eightClick() {
+    variables.innerHTML = eight.value;
+    console.log(eight.value);
+}
+
+function nineClick() {
+    variables.innerHTML = nine.value;
+    console.log(nine.value);
+}
+
+function splitClick() {
+    variables.innerHTML = split.value;
+    console.log(split.value);
+}
+
+function multiplyClick() {
+    variables.innerHTML = multiply.value;
+    console.log(multiply.value);
+}
+
+
+
+function minusClick() {
+    variables.innerHTML = minus.value;
+    console.log(minus.value);
+}
+
+
+
+
+
+function dotClick() {
+    variables.innerHTML = dot.value;
+    console.log(dot.value);
+}
+
+
+
+
+function plusClick() {
+    variables.innerHTML = plus.value;
+    console.log(plus.value);
+}
