d7d6412ffebf1f2c5978c1a37c47f544b7521eb9
”Mikhail <mikhail.hasilau@gmail.com>
2021-05-04 08:29:03 +0300

    i tak soidet

--- 18/index.js
+++ 18/index.js
@@ -7,22 +7,11 @@ const butt =  document.createElement('button');
 butt.innerHTML = 'Add';
 document.body.append(butt);
 
-
-function bgImg(){
-    style.backgroundImage = "url('https://i.stack.imgur.com/h8Blj.png')";
-}
-
-
 butt.onclick = () => {
     const ul =  document.createElement('ul');
     const li =  document.createElement('li');
     const butt1 = document.createElement('button');
-    // butt1.style.backgroundImage = "url('https://i.stack.imgur.com/h8Blj.png')";
-    // butt1.type = 'image';
-    // butt1.className = 'bgImg';
-    // butt1.src = 'https://pngicon.ru/file/uploads/128_16_2.png';
     butt1.innerHTML = 'Del';
-    butt1.setAttribute = 'bgImg';
     toDoArr.push(inp.value);
     toDoArr.forEach( (el, i) => {
         li.innerHTML = toDoArr[i];
